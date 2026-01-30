8487acc2fadc06955b4a76dbefae63a2bf538179
do_ur_homwork <109537113+4011v20c@users.noreply.github.com>
2026-01-30 15:52:46 -0500

    Add files via upload

---
+++ ccc02j1.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int digit;
+    cin >> digit;
+    if(digit==0){
+        cout << " * * *" << endl;
+        cout << "*     *" << endl;
+        cout << "*     *" << endl;
+        cout << "*     *" << endl;
+        cout << endl;
+        cout << "*     *" << endl;
+        cout << "*     *" << endl;
+        cout << "*     *" << endl;
+        cout << " * * *" << endl;
+    } else if (digit==1){
+        cout << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << endl;
+    } else if (digit==2){
+        cout << " * * *" << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << " * * *" << endl;
+        cout << "*" << endl;
+        cout << "*" << endl;
+        cout << "*" << endl;
+        cout << " * * *" << endl;
+    } else if (digit==3){
+        cout << " * * *" << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << " * * *" << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << " * * *" << endl;
+    } else if (digit==4){
+        cout << endl;
+        cout << "*     *" << endl;
+        cout << "*     *" << endl;
+        cout << "*     *" << endl;
+        cout << " * * *" << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << endl;
+    } else if (digit==5){
+        cout << " * * *" << endl;
+        cout << "*" << endl;
+        cout << "*" << endl;
+        cout << "*" << endl;
+        cout << " * * *" << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << " * * *" << endl;
+    } else if (digit==6){
+        cout << " * * *" << endl;
+        cout << "*" << endl;
+        cout << "*" << endl;
+        cout << "*" << endl;
+        cout << " * * *" << endl;
+        cout << "*     *" << endl;
+        cout << "*     *" << endl;
+        cout << "*     *" << endl;
+        cout << " * * *" << endl;
+    } else if (digit==7){
+        cout << " * * *" << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << endl;
+    } else if (digit==8){
+        cout << " * * *" << endl;
+        cout << "*     *" << endl;
+        cout << "*     *" << endl;
+        cout << "*     *" << endl;
+        cout << " * * *" << endl;
+        cout << "*     *" << endl;
+        cout << "*     *" << endl;
+        cout << "*     *" << endl;
+        cout << " * * *" << endl;
+    } else if (digit==9){
+        cout << " * * *" << endl;
+        cout << "*     *" << endl;
+        cout << "*     *" << endl;
+        cout << "*     *" << endl;
+        cout << " * * *" << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << "      *" << endl;
+        cout << " * * *" << endl;
+    }
+
+    return 0;
+}
